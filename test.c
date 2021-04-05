ee36f942b6b08a2e0496134acb49abcf1e80a3fb
Zainab  Dnaya <zdnaya@e2r12p1.1337.ma>
2021-04-05 14:27:28 +0100

    test-more

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 11:45:40 by zdnaya            #+#    #+#             */
-/*   Updated: 2021/04/05 14:12:01 by zdnaya           ###   ########.fr       */
+/*   Updated: 2021/04/05 14:24:42 by zdnaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@
 typedef struct s_test{
     int data;
     struct s_test *next;
+    struct s_tes *prv;
+
 } t_test;
+
 
 void refer(char **b)
 {
@@ -25,14 +28,58 @@ void refer(char **b)
     *b = "world";
 
 }
+void print_list(t_test *ptr)
+{
+    t_test *tmp;
+    // printf(" -------Printing list Start------- \n");
+    tmp = ptr;
+    while (ptr != NULL)
+    {
+        printf("[%d]\n", ptr->data);
+        printf("↑↓\n");
+        ptr = ptr->next;
+    }
+    ptr = tmp;
+    // printf(" -------Printing list End------- \n");
 
+    return;
+}
+void add(t_test **head, t_test *new, int data)
+{
+    t_test *tmp;
+
+    new = malloc(sizeof(t_test));
+    new->data = data;
+    new->next = NULL;
+    new->prv = NULL;
+    if (*head == NULL)
+        *head = new;
+    else
+    {
+        tmp = *head;
+        while (tmp->next)
+            tmp = tmp->next;
+        tmp->next = new;
+        new->prv = tmp;
+    }
+}
 int main()
 {
-    char *p;
 
-    p = "hello";
-    printf("befor : ==>%s\n", p);
-    refer(&p);
-    printf("after : ==>%s\n", p);
+    t_test *A;
+    t_test *B;
+    t_test *C;
+    t_test *D;
+    t_test *E;
+
+    A = malloc(sizeof(t_test));
+    A->data = 8;
+    A->next = NULL;
+    A->prv = NULL;
+    add(&A, B, 1);
+    add(&A, C, 9);
+    add(&A, D, -4);
+    add(&A, E, -85);
+    print_list(A);
     return 0;
 }
